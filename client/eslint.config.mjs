a69eb14e850b619bea9130105899b412236be764
katja981 <katstr01@easv365.dk>
2025-11-18 11:04:45 +0100

    Added EsLint, EsLint documentation and fixed the problems we had with the eslint fix function

---
+++ client/eslint.config.mjs
@@ -0,0 +1,24 @@
+﻿import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+
+export default defineConfig(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat['jsx-runtime'],
+
+    {
+        settings: {
+            react: {
+                version: 'detect'
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope' : 'off',
+        },
+    },
+    eslintConfigPrettier,
+)
