b5c9acc5281cb873b2154999037a69cd59b5758c
katja981 <katstr01@easv365.dk>
2025-11-24 10:41:15 +0100

    Added global API and error handling

---
+++ client/src/core/baseUrl.ts
@@ -0,0 +1,5 @@
+﻿const isProduction = import.meta.env.PROD;
+
+const prod = "https://3rdsemesterfinalproject.fly.dev";
+const dev = "https://localhost:5284";
+export const baseUrl = isProduction ? prod : dev;
---
+++ client/src/utilities/customFetch.ts
@@ -0,0 +1,26 @@
+﻿import type {ProblemDetails} from "../core/problemdetails.ts";
+import { toast } from "react-hot-toast";
+
+export const customFetch = {
+    fetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
+        const token = localStorage.getItem('jwt');
+        const headers = new Headers(init?.headers ?? {});
+        
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+        
+        return fetch(url, {
+            ...init,
+            headers,
+        }).then(async (response) => {
+            if (!response.ok) {
+                const errorClone = response.clone();
+                const problemDetails = (await errorClone.json()) as ProblemDetails;
+                console.log(problemDetails);
+                toast(problemDetails.title);
+            }
+            return response;
+        });
+    }
+};
