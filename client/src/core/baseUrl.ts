7b49a91db7ec68caa605905b203a05a4c697fe17
katja981 <katstr01@easv365.dk>
2025-11-25 11:49:00 +0100

    Dataacess 4

--- client/src/core/baseUrl.ts
+++ client/src/core/baseUrl.ts
@@ -6,6 +6,6 @@ const isProduction = import.meta.env.PROD;
 const prod = "https://deadpigeons.fly.dev";
 const dev = "http://localhost:5173";
 
-export const finalUrl = isProduction ? prod : dev;
+export const baseUrl = isProduction ? prod : dev;
 
-export const todoClient = new AuthClient(finalUrl)
+export const authClient = new AuthClient(baseUrl)
