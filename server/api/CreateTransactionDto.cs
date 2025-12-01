224ffc829f1ada1e09771102c12b67621ba6399e
katja981 <katstr01@easv365.dk>
2025-12-01 09:50:50 +0100

    Added Transaction endpoint

---
+++ server/api/CreateTransactionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace api;
+
+public class CreateTransactionDto
+{
+    public Guid PlayerId { get; set; }
+    public int Amount { get; set; }
+    public string MobilePayTransactionNumber { get; set; }
+}
--- server/api/TransactionController.cs
+++ server/api/TransactionController.cs
@@ -1,6 +1,82 @@
-﻿namespace api;
+﻿using dataaccess.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api;
 
 public class TransactionController
 {
+    private readonly ITransactionService _transactionService;
     
+    public TransactionController(ITransactionService transactionService)
+    {
+        _transactionService = transactionService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Transaction>>> GetAll()
+    {
+        var transactions = await _transactionService.GetAllAync();
+        return Ok(transactions);
+    }
+
+    [HttpGet("player/{playerId:guid}/balance")]
+    public async Task<ActionResult<decimal>> GetBalance([FromRoute] Guid playerId)
+    {
+        var balance = await _transactionService.GetBalanceAsync(playerId);
+        return Ok(balance);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Transaction>> Create([FromBody] CreateTransactionDto createTransactionDto)
+    {
+        try
+        {
+            var t = await _transactionService.CreatePendingAsync(
+                createTransactionDto.PlayerId,
+                createTransactionDto.Amount,
+                createTransactionDto.MobilePayTransactionNumber
+                );
+            return Ok(t);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost("{id:guid}/approve")]
+    public async Task<IActionResult> Approve([FromRoute] Guid id)
+    {
+        try
+        {
+            await _transactionService.ApproveAsync(id);
+            return Ok();
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost("{id:guid}/reject")]
+    public async Task<IActionResult> Reject([FromRoute] Guid id)
+    {
+        try
+        {
+            await _transactionService.RejectAsync(id);
+            return Ok();
+        }
+        catch  (KeyNotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
