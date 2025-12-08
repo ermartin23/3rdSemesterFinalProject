03030249854feb400d31130b4d5d6727c1519123
lauraaa05 <laushp01@easv365.dk>
2025-12-08 09:38:58 +0100

    Small changes for board classes

--- server/api/Features/Boards/Dtos/CreateBoardRequest.cs
+++ server/api/Features/Boards/Dtos/CreateBoardRequest.cs
@@ -4,7 +4,7 @@ public class CreateBoardRequest
 {
     public Guid PlayerId { get; set; }
     public Guid GameId { get; set; }
-    public List<int> ChosenNumbers { get; set; }
+    public List<int> ChosenNumbers { get; set; } = new();
     public bool IsWinningBoard { get; set; }
     public decimal Price { get; set; }
     public Guid? RepeatingBoardId { get; set; }
