cd1dbec11d259fa7a46e7950a858f55784c110b5
lauraaa05 <laushp01@easv365.dk>
2025-12-18 17:05:10 +0100

    Fixed boards with now using backend and saving to database. Also fixed the history to show every board purchased

--- client/src/core/generated-client.ts
+++ client/src/core/generated-client.ts
@@ -994,6 +994,39 @@ export class BoardClient {
         }
         return Promise.resolve<FileResponse>(null as any);
     }
+
+    getMyBoards(): Promise<BoardHistoryResponse[]> {
+        let url_ = this.baseUrl + "/api/boards/me";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processGetMyBoards(_response);
+        });
+    }
+
+    protected processGetMyBoards(response: Response): Promise<BoardHistoryResponse[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as BoardHistoryResponse[];
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<BoardHistoryResponse[]>(null as any);
+    }
 }
 
 export class AuthClient {
@@ -1363,6 +1396,17 @@ export interface UpdateBoardRequest {
     chosenNumbers?: number[] | undefined;
     isWinningBoard?: boolean | undefined;
     price?: number | undefined;
+}
+
+export interface BoardHistoryResponse {
+    boardId?: string;
+    gameId?: string;
+    weekIdentity?: string;
+    week?: number;
+    year?: number;
+    chosenNumbers?: number[];
+    price?: number;
+    repeatingBoardId?: string | undefined;
 }
 
 export interface LoginResponseDto {
